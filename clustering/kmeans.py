3d861146657c4a0437d3c6c6afbc5a3e5b442443
R-Suresh <26441234+R-Suresh@users.noreply.github.com>
2019-09-29 21:35:42 -0400

    added clustering code

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
 from math import inf
+import math
+import numpy as np
 import random
 import csv
+import pandas as pd
 
 
 def point_avg(points):
@@ -11,7 +14,12 @@ def point_avg(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    mean=np.zeros_like(points[0])
+    for point in points:
+        mean+=point
+    mean=mean/len(points)
+    return mean
+    # raise NotImplementedError()
 
 
 def update_centers(data_set, assignments):
@@ -21,11 +29,23 @@ def update_centers(data_set, assignments):
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+
+    centers=[]
+    for label in np.unique(assignments):
+        cluster=[]
+        for i, assignment in enumerate(assignments):
+            if(assignment==label):
+                cluster.append(data_set[i])
+        centers.append(point_avg(cluster))
+    # return the center for each group
+    return centers
+    # raise NotImplementedError()
 
 
 def assign_points(data_points, centers):
     """
+    Input : data points and the array representing centres for all the clusters
+    Output : returns the assigment for each point in the dataset
     """
     assignments = []
     for point in data_points:
@@ -44,7 +64,8 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    return math.sqrt(np.sum(np.square(np.array(a)-np.array(b))))
+    # raise NotImplementedError()
 
 
 def generate_k(data_set, k):
@@ -52,15 +73,38 @@ def generate_k(data_set, k):
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    chosen_points = [data_set[random.randint(0, len(data_set) - 1)] for y in range(1, k)]
+    return chosen_points
+    # raise NotImplementedError()
 
 
 def get_list_from_dataset_file(dataset_file):
-    raise NotImplementedError()
+    '''
+    param dataset_file: file whci represents the dataset
+    return: returns data points from the file
+    '''
+    df=pd.read_csv(dataset_file)
+    return df.values
+    # raise NotImplementedError()
 
 
 def cost_function(clustering):
-    raise NotImplementedError()
+    '''
+    param clustering: dict of assignments and points
+    return: the mean euclidean loss for the entire clustering
+    '''
+    points=[]
+    assignments=[]
+    for key, values in clustering.items():
+        for value in values:
+            assignments.append(key)
+            points.append(value)
+        centers=update_centers(points,assignments)
+    cost=0
+    for i,point in enumerate(points):
+        cost+=distance(point,centers[assignments[i]-1])
+    return cost/len(points)
+    # raise NotImplementedError()
 
 
 def k_means(dataset_file, k):
